01a39c9244173956405504222052a28350d72a69
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-05-29 19:30:56 -0500

    con funciones de Pao, no se me ocurre lo de las publicaiones

--- modelo/PHP/perfil_usuario.php
+++ modelo/PHP/perfil_usuario.php
@@ -1,17 +1,14 @@
 <?php
-
+  include "funciones.php";
   $bd = mysqli_connect("localhost","root","","truequep6");
+  checar_con($bd);
 
-  if(!$bd){
-		echo mysqli_connect_error();
-		echo mysqli_connect_errno();
-    echo "no es posible conectarse con la base de datos";
-		exit();
-	}
   $id_usuario= 317341612;//Número de cuenta del usuario, obtener de session
 
   $perfil = array();
   $resp = mysqli_query($bd, "SELECT nomus, imagen FROM usuario WHERE id_usuario = $id_usuario");
   $perfil = mysqli_fetch_assoc($resp);
+
+  // Obtener publicaciones :)
 
 ?>
