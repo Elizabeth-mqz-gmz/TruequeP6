be9f635cb9c727b24a1a9f583c2caa98e0b63c27
paoinnes <paolainnes@comunidad.unam.mx>
2018-05-29 19:04:30 -0500

    funciones viejitas mejoradas jsjs

--- controlador/PHP/funciones.php
+++ controlador/PHP/funciones.php
@@ -1,16 +1,21 @@
 function validar ($pal,$regex){//Esta función recibe una palabra y una regex
       $pal=strip_tags($pal); //quitar las etiquetas
-      $pal=mysql_real_escape_string($conex,$pal); //quitar los comandos sql
+      $pal=mysqli_real_escape_string($conex,$pal); //quitar los comandos sql
       $val=preg_match($regex,$pal); //checar la coincidencia
-      if ($val==1)
-        return $pal; //coincide con la regex, devuelve la cadena validada y segura
+      if ($regex != ""){
+        if ($val==1)
+          return $pal; //coincide con la regex, devuelve la cadena validada y segura
+        else
+          return false; //regresa falso porque no cumple con la regex
+      }
       else
-        return false; //regresa falso porque no cumple con la regex
-  }
+        return $pal;
+}
+
 function checar_con($conex){//revisa conexión escribe en pantalla si hay error //testing
 		if(!$conex){
 			echo mysqli_connect_error();
 			echo mysqli_connect_errno();
 			exit();
 		}
-	}
+}
