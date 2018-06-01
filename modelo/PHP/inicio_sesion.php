2c1db4b6f8c82aac7208465682b65241f8459241
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-06-01 13:04:35 -0500

    el inicio de sesion ya está

--- modelo/PHP/inicio_sesion.php
+++ modelo/PHP/inicio_sesion.php
@@ -6,14 +6,17 @@
   $datosUsu = array ("id_usuario" => $_POST["id_usuario"] , "contra" => $_POST["contra"]);
   $bd = mysqli_connect("localhost", "root", "", "truequep6");
   checar_con($bd);
+
   forEach($datosUsu as $ind => $ele)
     $datosUsu[$ind]= validar($ele,"", $bd);//escapar datos ingresados por el usuario
+
   $bus = "SELECT id_usuario, contra FROM usuario WHERE id_usuario = ".$datosUsu["id_usuario"];
   $resp = mysqli_query($bd, $bus);
   $bus = mysqli_fetch_assoc($resp);
-  //print_r($bus);
-  if($bus != null){
-    if($bus["contra"]== $datosUsu["contra"]){//falta hacer hash
+
+  if($bus != null){//solo si existe coincidencia verifica que la contraseña sea la correcta
+    $datosUsu["contra"]=$hash = sha1("f2H3".$datosUsu["contra"]."B6jwq");//hace el hash
+    if($bus["contra"]== $datosUsu["contra"]){
       echo "T";
       if ($datosUsu["id_usuario"] == 979847874) //revisa si coincide con el id de administrador
         echo "T";
