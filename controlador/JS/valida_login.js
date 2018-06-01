dc7df9d28c1e46944896e36f0b64054eb16c875b
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-06-01 14:35:36 -0500

    solo falta quitar denuncias

---
+++ controlador/JS/valida_login.js
@@ -0,0 +1,29 @@
+$("#enviar").click(()=>{
+    $("#alert").remove();
+    var usuario = $("#num_cta").val();
+    var contraseña = $("#pass").val();
+    jQuery.ajax({
+        url:"../../modelo/PHP/inicio_sesion.php",
+        data:{
+            id_usuario : usuario,
+            contra : contraseña
+        },
+        type: "POST",
+        success: function(response){
+            let n = response;
+            if (n.length == 2){
+              //cookie que indica que eres un administrador
+              window.location="index_pru.html";
+            }
+            else{
+              if (n == "T"){
+                window.location="index_pru.html";
+              }
+              else{
+              var alerta = $("<div id = 'alert' class='alert alert-danger' role='alert'>Tu número de cuenta o contraseña no son validos. Por favor intentalo de nuevo.</div>");
+              alerta.appendTo("#mensaje");
+              }
+            }
+        }
+    });
+});
