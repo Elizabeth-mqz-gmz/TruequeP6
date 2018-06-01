7f8a4ad99a00a053ad0a69b81db145a0b8ac4a95
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-06-01 12:32:22 -0500

    ya revisa si existe el el usuario en la BD, y si es Admi = 979847874

--- modelo/PHP/inicio_sesion.php
+++ modelo/PHP/inicio_sesion.php
@@ -1,12 +1,14 @@
 <?php
-//inicio_sesion.php
+//inicio_esion.php
+  extract($_POST);
+
   include "funciones.php";
   $datosUsu = array ("id_usuario" => $_POST["id_usuario"] , "contra" => $_POST["contra"]);
   $bd = mysqli_connect("localhost", "root", "", "truequep6");
   checar_con($bd);
   forEach($datosUsu as $ind => $ele)
     $datosUsu[$ind]= validar($ele,"", $bd);//escapar datos ingresados por el usuario
-  $bus = "SELECT id_usuario, contra FROM usuario WHERE id_usuario = ".$datosUsu["id_usuario"]; //query bonita, pero no tanto como la de calnedario XD
+  $bus = "SELECT id_usuario, contra FROM usuario WHERE id_usuario = ".$datosUsu["id_usuario"];
   $resp = mysqli_query($bd, $bus);
   $bus = mysqli_fetch_assoc($resp);
   //print_r($bus);
@@ -14,7 +16,7 @@
     if($bus["contra"]== $datosUsu["contra"]){//falta hacer hash
       echo "T";
       if ($datosUsu["id_usuario"] == 979847874) //revisa si coincide con el id de administrador
-        echo ",Admi";
+        echo "T";
       }
     else
       echo "F";
