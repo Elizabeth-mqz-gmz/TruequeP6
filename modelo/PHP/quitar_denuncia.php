3ccd8017bd9ab8f6497224f27707a265b164884c
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-06-01 16:31:01 -0500

    falta hacer ajax para ejecutar la acción

--- modelo/PHP/quitar_denuncia.php
+++ modelo/PHP/quitar_denuncia.php
@@ -1,3 +1,20 @@
 <?php
 // quitar denuncia
+ include "funciones.php";
+ $bd = mysqli_connect("localhost", "root", "", "truequep6");
+ checar_con($bd);
+
+ $publicacion = validar($_POST["publi"], "", $bd);
+ $usuario = validar($_POST["usuario"], "", $bd);
+
+ $bus = "UPDATE publicacion SET denuncia_p = '0' WHERE id_publicacion = $publicacion"; //quitar denuncia
+ mysqli_query($bd, $bus);
+
+ $mensaje = "Fue retirada una denuncia.";
+ $bus = "INSERT INTO notificacion (id_usu_not, men_not) VALUES ($usuario, '$mensaje')";
+ mysqli_query($bd, $bus);
+
+ echo "Cambio realizado con exito";
+
+ mysqli_close($bd);
 ?>
