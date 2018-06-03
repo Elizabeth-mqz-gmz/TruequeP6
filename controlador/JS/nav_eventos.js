72868beffd766cf61350301e03773b52f0751cdd
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-06-03 09:33:02 -0500

    nada

--- controlador/JS/nav_eventos.js
+++ controlador/JS/nav_eventos.js
@@ -15,6 +15,23 @@ var click = 0;
     eliminar_eventos();
   });
 
+  $("body").on("load", function(){
+    revisaCookie();
+  });
+
+  function revisaCookie(){
+    jQuery.ajax({
+        url:"../../modelo/PHP/cookieAdmi.php",
+        type: "POST",
+        data:{
+        },
+        success: function(response){
+          n = response;
+          console.log(response);
+        }
+    });
+  }
+
 var publi = 0; //Saber en que html está, al inicio se encuentra en Trueque
   $('.cambio').on('click',()=>{
     boton = (publi == 0)? 1 : 0;
