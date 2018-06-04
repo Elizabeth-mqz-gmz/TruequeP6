9f22d31fcd59ffeb8719bc8c9e2b7b1a46aa23f7
Elizabeth-mqz-gmz <elimqzgmzhp@comunidad.unam.mx>
2018-06-03 19:23:31 -0500

    Avance

--- modelo/PHP/denuncia.php
+++ modelo/PHP/denuncia.php
@@ -12,7 +12,7 @@
     }
     $idPub = $form["idPubli"];
     $denuncia = $form["motivo"];
-
+    validar($denuncia,"/[A-Za-z\d]{20,200}$/",$db);
     //actualiza el estado de una publicación, y cambia a 1 el atributo denuncia
     $upd = "UPDATE publicacion SET denuncia_p='1',razon_denuncia='$denuncia' WHERE id_publicacion='$idPub'";
     mysqli_query($db,$upd);
